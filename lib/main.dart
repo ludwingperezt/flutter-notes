214caf9c7e83810fc1083c110b871721b6d65284
Ludwing Perez <mirage232@gmail.com>
2023-04-15 12:22:59 -0600

    Implementar Widget Binding para iniciarlizar la app Firebase antes de iniciar la app.

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,10 @@ void main() async {
   // Cargar el archivo de variables de entorno.
   await dotenv.load(fileName: ".env");
 
+  // Se habilita Wigdet Binding antes de iniciar Firebase
+  // https://docs.flutter.dev/resources/architectural-overview#architectural-layers
+  WidgetsFlutterBinding.ensureInitialized();
+
   // Al mover la craeción del Theme de la app (que es un objeto MaterialApp)
   // se evita que este sea creado cada vez que se guardan cambios, lo que ahorra
   // recursos.
@@ -68,43 +72,71 @@ class _InicioPageState extends State<InicioPage> {
       appBar: AppBar(
         title: const Text('Inicio'),
       ),
-      body: Column(
-        children: [
-          TextField(
-            controller: _email,
-            enableSuggestions: false,
-            autocorrect: false,
-            keyboardType: TextInputType.emailAddress,
-            decoration: const InputDecoration(
-              hintText: 'Ingrese su email',
-            ),
-          ),
-          TextField(
-            controller: _password,
-            obscureText: true,
-            enableSuggestions: false,
-            autocorrect: false,
-            decoration: const InputDecoration(
-              hintText: 'Ingrese su contraseña',
-            ),
-          ),
-          TextButton(
-            onPressed: () async {
-              await Firebase.initializeApp(
-                options: DefaultFirebaseOptions.currentPlatform,
+      body: FutureBuilder(
+        future: Firebase.initializeApp(
+          options: DefaultFirebaseOptions.currentPlatform,
+        ),
+        // FutureBuilder se usa para inicializar un widget HASTA que una promesa se haya completado, en este caso hasta que la app Firebase se haya iniciado.
+        builder: (context, snapshot) {
+          // a través de snapshot se puede obtener el estado de la promesa (Future)
+          // y realizar alguna acción mientras se espera que se resuelva y cuando
+          // se reciba una respuesta, ya sea exitosa o sea un error.
+
+          switch (snapshot.connectionState) {
+            // TODO: Pendiente de implementar los otros casos de estado del Future
+            // case ConnectionState.none:
+            //   // TODO: Handle this case.
+            //   break;
+            // case ConnectionState.waiting:
+            //   // TODO: Handle this case.
+            //   break;
+            // case ConnectionState.active:
+            //   // TODO: Handle this case.
+            //   break;
+
+            case ConnectionState.done:
+              // El widget Column se usa para apilar componentes, en este caso
+              // se apilan los campos de usuario, contraseña y botón de registro
+              // en una sola columna.
+              return Column(
+                children: [
+                  TextField(
+                    controller: _email,
+                    enableSuggestions: false,
+                    autocorrect: false,
+                    keyboardType: TextInputType.emailAddress,
+                    decoration: const InputDecoration(
+                      hintText: 'Ingrese su email',
+                    ),
+                  ),
+                  TextField(
+                    controller: _password,
+                    obscureText: true,
+                    enableSuggestions: false,
+                    autocorrect: false,
+                    decoration: const InputDecoration(
+                      hintText: 'Ingrese su contraseña',
+                    ),
+                  ),
+                  TextButton(
+                    onPressed: () async {
+                      // Aqui vamos a crear el usuario en firebase.
+                      final email = _email.text;
+                      final password = _password.text;
+
+                      final userCredentials = await FirebaseAuth.instance
+                          .createUserWithEmailAndPassword(
+                              email: email, password: password);
+                    },
+                    child: const Text('Registrarse'),
+                  ),
+                ],
               );
 
-              // Aqui vamos a crear el usuario en firebase.
-              final email = _email.text;
-              final password = _password.text;
-
-              final userCredentials = await FirebaseAuth.instance
-                  .createUserWithEmailAndPassword(
-                      email: email, password: password);
-            },
-            child: const Text('Registrarse'),
-          ),
-        ],
+            default:
+              return const Text('Cargando...');
+          }
+        },
       ),
     );
   }
