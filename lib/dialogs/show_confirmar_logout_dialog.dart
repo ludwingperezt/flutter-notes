480552ef8b1c5bfd3767f4b4ac7da929db734aee
Ludwing Perez <mirage232@gmail.com>
2023-04-25 12:14:49 -0600

    Refactorizar dialog de confirmación para cerrar sesión.

---
+++ lib/dialogs/show_confirmar_logout_dialog.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+/// Modal que sirve para confirmar el cierre de la sesión
+Future<bool> mostrarLogOutDialog(BuildContext context) {
+  return showDialog<bool>(
+    context: context,
+    builder: (context) {
+      return AlertDialog(
+        title: const Text('Salir'),
+        content: const Text('Está seguro de cerrar sesión?'),
+        actions: [
+          TextButton(
+              onPressed: () {
+                // Indicar que el usuario canceló el modal
+                Navigator.of(context).pop(false);
+              },
+              child: const Text('Cancelar')),
+          TextButton(
+              onPressed: () {
+                // Indicar que el usuario confirmó el cierre de la sesión
+                Navigator.of(context).pop(true);
+              },
+              child: const Text('Log out')),
+        ],
+      );
+    },
+  ).then((value) =>
+      value ??
+      false); // esta línea es para especificar un valor de retorno cuando el usuario descarta el popup con el botón "Atras" o cuando hace clic en la pantalla para cerrar el diálogo.
+}
--- lib/views/notas_view.dart
+++ lib/views/notas_view.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:mynotes/constants/routes.dart';
+import 'package:mynotes/dialogs/show_confirmar_logout_dialog.dart';
 import 'package:mynotes/enums/menu_action.dart';
 import 'dart:developer' as devtools show log;
 
@@ -59,33 +60,4 @@ class _NotasViewState extends State<NotasView> {
       body: const Text('Bienvenido!'),
     );
   }
-}
-
-/// Modal que sirve para confirmar el cierre de la sesión
-Future<bool> mostrarLogOutDialog(BuildContext context) {
-  return showDialog<bool>(
-    context: context,
-    builder: (context) {
-      return AlertDialog(
-        title: const Text('Salir'),
-        content: const Text('Está seguro de cerrar sesión?'),
-        actions: [
-          TextButton(
-              onPressed: () {
-                // Indicar que el usuario canceló el modal
-                Navigator.of(context).pop(false);
-              },
-              child: const Text('Cancelar')),
-          TextButton(
-              onPressed: () {
-                // Indicar que el usuario confirmó el cierre de la sesión
-                Navigator.of(context).pop(true);
-              },
-              child: const Text('Log out')),
-        ],
-      );
-    },
-  ).then((value) =>
-      value ??
-      false); // esta línea es para especificar un valor de retorno cuando el usuario descarta el popup con el botón "Atras" o cuando hace clic en la pantalla para cerrar el diálogo.
 }
