7b7c89eb9fe8189934c41ad2310eb67cbde1f382
Ludwing Perez <mirage232@gmail.com>
2023-04-27 11:11:19 -0600

    Modificar la vista de notas.
    * Se hizo un FutureBuilder para asegurar que se guarda u obtiene el
      email del usuario en la base de datos local.
    * También se creó el StreamBuilder para listar todas las notas
      guardads en la base de datos.

--- lib/views/notas_view.dart
+++ lib/views/notas_view.dart
@@ -5,6 +5,7 @@ import 'package:mynotes/enums/menu_action.dart';
 import 'dart:developer' as devtools show log;
 
 import 'package:mynotes/services/auth/auth_service.dart';
+import 'package:mynotes/services/crud/notas_service.dart';
 
 class NotasView extends StatefulWidget {
   const NotasView({super.key});
@@ -14,6 +15,23 @@ class NotasView extends StatefulWidget {
 }
 
 class _NotasViewState extends State<NotasView> {
+  // Exponer el email del usuario loggeado
+  String get userEmail => AuthService.firebase().currentUser!.email!;
+
+  late final NotasService _notasService;
+
+  @override
+  void initState() {
+    _notasService = NotasService();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _notasService.cerrarDB();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -58,7 +76,52 @@ class _NotasViewState extends State<NotasView> {
           )
         ],
       ),
-      body: const Text('Bienvenido!'),
+      body: FutureBuilder(
+        // Con el FutureBuilder se hace una suscripción al Future para insertar
+        // o crear el registro del email del usuario en la base de datos local.
+        // Dependendo del estado del Future se renderizará la lista de notas
+        // a través de un StreamBuilder o un Progress Indicator mientras se
+        // espera una respuesta.
+        future: _notasService.obtenerCrearUser(email: userEmail),
+        builder: (context, snapshot) {
+          switch (snapshot.connectionState) {
+            // case ConnectionState.none:
+            //   // TODO: Handle this case.
+            //   break;
+            // case ConnectionState.waiting:
+            //   // TODO: Handle this case.
+            //   break;
+            // case ConnectionState.active:
+            //   // TODO: Handle this case.
+            //   break;
+            case ConnectionState.done:
+              // Para mostrar todas las notas se usa un StreamBuilder que se
+              // suscribe al stream para obtener todas las notas.
+              return StreamBuilder(
+                stream: _notasService.todasNotas,
+                builder: ((context, snapshot) {
+                  switch (snapshot.connectionState) {
+                    // case ConnectionState.none:
+                    //   // TODO: Handle this case.
+                    //   break;
+                    case ConnectionState.waiting:
+                      return const Text('Esperando cargar todas las notas...');
+                      // case ConnectionState.active:
+                      //   // TODO: Handle this case.
+                      //   break;
+                      // case ConnectionState.done:
+                      //   // TODO: Handle this case.
+                      break;
+                    default:
+                      return const CircularProgressIndicator();
+                  }
+                }),
+              );
+            default:
+              return const CircularProgressIndicator();
+          }
+        },
+      ),
     );
   }
 }
