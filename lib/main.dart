2fa1f9175d066c1b13aa0c4a2f1ee2336f250961
Ludwing Perez <mirage232@gmail.com>
2023-05-01 09:53:10 -0600

    Agregar el BlocProvider en el archivo main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:mynotes/constants/routes.dart';
+import 'package:mynotes/services/auth/bloc/auth_bloc.dart';
+import 'package:mynotes/services/auth/firebase_auth_provider.dart';
 import 'package:mynotes/views/inicio_view.dart';
 import 'package:mynotes/views/login_view.dart';
 import 'package:mynotes/views/notas/crear_editar_nota_view.dart';
@@ -8,6 +11,7 @@ import 'package:mynotes/views/notas_view.dart';
 import 'package:mynotes/views/registro_view.dart';
 import 'package:mynotes/views/verificar_email_view.dart';
 
+/// La función main() NO ES INVOCADA durante los hot reloads.
 void main() async {
   // Cargar el archivo de variables de entorno.
   await dotenv.load(fileName: ".env");
@@ -34,7 +38,14 @@ void main() async {
         // is not restarted.
         primarySwatch: Colors.amber,
       ),
-      home: const InicioView(),
+      home: BlocProvider<AuthBloc>(
+        // Aqui se usa BlocProvider para inicializar el bloc AuthBloc.
+        // En este caso al context que se está pasando a la inline function
+        // se le está inyectando el AuthBloc para que esté disponible para toda
+        // la app.
+        create: (context) => AuthBloc(FirebaseAuthProvider()),
+        child: const InicioView(),
+      ),
       // Creación de las rutas de la app:
       routes: {
         loginRoute: (context) => const LoginView(),
