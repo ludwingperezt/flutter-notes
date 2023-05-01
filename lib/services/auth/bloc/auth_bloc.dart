656c5dabed50d426955f9a299ba14a4a503799f1
Ludwing Perez <mirage232@gmail.com>
2023-05-01 13:31:48 -0600

    Refactorizar AuthBloc.
    * Se arreglaron los eventos de inicialización, login y logout porque
      tenían errores.
    * Se agregaron los listeners y handlers de los eventos de envío de email de
      verificación y el de registro de usuarios.

--- lib/services/auth/bloc/auth_bloc.dart
+++ lib/services/auth/bloc/auth_bloc.dart
@@ -5,14 +5,16 @@ import 'package:mynotes/services/auth/bloc/auth_state.dart';
 
 class AuthBloc extends Bloc<AuthEvent, AuthState> {
   // Todo bloc class debe tener un estado inicial
-  AuthBloc(AuthProvider provider) : super(const AuthStateLoading()) {
+  AuthBloc(AuthProvider provider) : super(const AuthStateUninitialized()) {
     // evento de incialización
     on<AuthEventInitialize>((event, emit) async {
       await provider.initialize();
       final user = provider.currentUser;
 
       if (user == null) {
-        emit(const AuthStateLoggedOut(null));
+        // al inicio, cuando la aplicación carga, el usuario no está por defecto
+        // y tampoco se está cargando nada y no hay excepciones.
+        emit(const AuthStateLoggedOut(exception: null, isLoading: false));
       } else if (!user.isEmailVerified) {
         emit(const AuthStateNeedsVerification());
       } else {
@@ -22,30 +24,74 @@ class AuthBloc extends Bloc<AuthEvent, AuthState> {
 
     // Evento de login
     on<AuthEventLogIn>((event, emit) async {
+      //
+      emit(
+        const AuthStateLoggedOut(
+          exception: null,
+          isLoading: true,
+        ),
+      );
+
       final email = event.email;
       final password = event.password;
 
       try {
         final user = await provider.logIn(email: email, password: password);
-        emit(AuthStateLoggedIn(user));
+
+        if (!user.isEmailVerified) {
+          // Si el usuario no tiene verificado su email, mantener al usuario
+          // NO loggeado y enviar el evento para verificar email.
+          emit(const AuthStateLoggedOut(exception: null, isLoading: false));
+          emit(const AuthStateNeedsVerification());
+        } else {
+          emit(const AuthStateLoggedOut(exception: null, isLoading: false));
+          emit(AuthStateLoggedIn(user));
+        }
       } on Exception catch (e) {
         // En este bloque es necesario colocar on Exception catch (e) porque
         // en dart cualquier objeto puede ser lanzado como una excepción, pero
         // el estado AuthStateLoginFailure solo acepta objetos que sean derivados
         // de Exception.
-        emit(AuthStateLoggedOut(e));
+        emit(AuthStateLoggedOut(
+          exception: e,
+          isLoading: false,
+        ));
       }
     });
 
     // Evento logout
     on<AuthEventLogOut>((event, emit) async {
       try {
-        emit(const AuthStateLoading());
         await provider.cerrarSesion();
-        emit(const AuthStateLoggedOut(null));
+        emit(const AuthStateLoggedOut(exception: null, isLoading: false));
       } on Exception catch (e) {
-        emit(AuthStateLogoutFailure(e));
+        emit(AuthStateLoggedOut(exception: e, isLoading: false));
       }
     });
+
+    // Manejo del evento cuando se envía el email de verificación.
+    on<AuthEventSendEmailVerification>((event, emit) async {
+      await provider.enviarEmailVerificacion();
+      // Cuando se envía el email de verificación no hay cambio de estado.
+      emit(state);
+    });
+
+    // Manejo del evento de registro de nuevo usuario.
+    on<AuthEventRegister>(
+      (event, emit) async {
+        final email = event.email;
+        final password = event.password;
+        try {
+          // Registrar al usuario y luego inmediatamente después, enviar el email
+          // de verificación.
+          await provider.crearUsuario(email: email, password: password);
+          await provider.enviarEmailVerificacion();
+
+          emit(const AuthStateNeedsVerification());
+        } on Exception catch (e) {
+          emit(AuthStateRegistering(e));
+        }
+      },
+    );
   }
 }
